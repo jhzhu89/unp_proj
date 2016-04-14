b5aa3ee4034e14f83345866071f3514a4a43ee9b
ZhuJiahao <jhzhu@ZhuJiahaos-MacBook-Pro.local>
2016-04-13 22:15:02 -0700

    add udp_client

---
+++ libunp/include/udp_client.h
@@ -0,0 +1,10 @@
+//
+// Created by ZhuJiahao on 4/13/16.
+//
+
+#ifndef UNP_PROJ_UDP_CLIENT_H
+#define UNP_PROJ_UDP_CLIENT_H
+
+int Udp_client(const char *, const char *, SA **, socklen_t *);
+
+#endif //UNP_PROJ_UDP_CLIENT_H
